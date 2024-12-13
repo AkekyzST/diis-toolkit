69e34897ddb0ae0476ea7ef3db5daeb2c4bbb86d
FSWRP <pangponii125@gmail.com>
2024-12-13 16:13:15 +0700

    .

--- DiiS.Toolkit/MyApplication/MyApplication/Program.cs
+++ DiiS.Toolkit/MyApplication/MyApplication/Program.cs
@@ -1,26 +1,25 @@
-using Cropper.Blazor.Extensions;
 using MudBlazor.Services;
-using Toolkit.Website.Components;
+using MyApplication.Client.Pages;
+using MyApplication.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
+// Add MudBlazor services
 builder.Services.AddMudServices();
 
-builder.Services.AddCropper();
-builder.Services.AddServerSideBlazor()
-    .AddHubOptions(options =>
-    {
-        options.MaximumReceiveMessageSize = 32 * 1024 * 100;
-    });
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -33,6 +32,8 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(MyApplication.Client._Imports).Assembly);
 
 app.Run();
--- New folder/Toolkit.Website/wwwroot/js/site.js
+++ New folder/Toolkit.Website/wwwroot/js/site.js
@@ -1,1 +1,29 @@
-﻿// Write your Javascript code.
+﻿let cropperInstance = null;
+
+function initializeCropper(imageElement) {
+    if (cropperInstance) {
+        cropperInstance.destroy(); // ลบ instance เดิมถ้ามี
+    }
+    cropperInstance = new Cropper(imageElement, {
+        aspectRatio: 1, // กำหนดสัดส่วน (เปลี่ยนตามความต้องการ เช่น 1 สำหรับสี่เหลี่ยมจตุรัส)
+        viewMode: 2, // โหมดการแสดงผล
+        autoCropArea: 0.8, // ขนาดพื้นที่ที่ถูกครอบอัตโนมัติ (0.8 = 80%)
+        movable: true, // เปิดใช้งานการย้ายรูปภาพ
+        zoomable: true, // เปิดใช้งานการซูม
+        rotatable: true, // เปิดใช้งานการหมุน
+        scalable: true, // เปิดใช้งานการปรับขนาด
+    });
+}
+
+function getCroppedImage() {
+    if (!cropperInstance) {
+        console.error("Cropper instance is not initialized.");
+        return null;
+    }
+
+    // แปลงภาพที่ครอปเป็น Base64
+    return cropperInstance.getCroppedCanvas().toDataURL();
+} function getCroppedCanvasDataURL() {
+    const cropper = document.querySelector('.cropper-container').cropper;
+    return cropper.getCroppedCanvas().toDataURL();
+}
