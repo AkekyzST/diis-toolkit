84df58f987db083367121f6ecfc3519ea407a1fa
EKAPHON TREENETWORAKHUN <ekaphon.t@psu.ac.th>
2025-01-07 14:47:33 +0700

    Init

--- DiiS.Toolkit/obj/Debug/net8.0/DiiS.Toolkit.AssemblyInfo.cs
+++ DiiS.Toolkit/obj/Debug/net8.0/DiiS.Toolkit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiiS.Toolkit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3135d9bbfe581f25916bbbc994456e49f155e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiiS.Toolkit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiiS.Toolkit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
